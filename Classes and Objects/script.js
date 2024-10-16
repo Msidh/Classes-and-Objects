03ba659e990ff532ff34f262175c377dc237ce95
Msidh <mehtaabsidhu23@gmail.com>
2024-10-16 19:02:36 +0100

    Add files via upload

---
+++ Classes and Objects/script.js
@@ -0,0 +1,17 @@
+class Movie {
+    constructor(title,director,year) {
+    this.title = title
+    this.director = director
+    this.year = year
+        }
+    }
+
+var def_movies = [
+    {title: 'Jurrasic Park', director: 'John Doe', year: '1990' },
+    {title: 'The Dead Pool', director: 'Mathew Albison', year: '2014'},
+    {title: 'The Avengers', director: 'Joss Whedon', year: '2012'}
+]
+
+class UI {
+    var id =  document.getElementById('movie-list')
+}
